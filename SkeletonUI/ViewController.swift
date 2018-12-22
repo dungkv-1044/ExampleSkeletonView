a86e21000fc286ec558da36319bdeddfa15acb09
Dung Khuat <dungkv.iosdev@gmail.com>
2018-12-22 23:37:33 +0700

    Initial Commit

---
+++ SkeletonUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkeletonUI
+//
+//  Created by Dung on 12/22/18.
+//  Copyright © 2018 Dung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
